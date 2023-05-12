5d2983abc08e632fccd57de12e0ae0cc63acd2b3
theaminnouri <theaminnouri@gmail.com>
2023-05-12 18:56:34 +0330

    add staticCheck

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,42 @@
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.detekt) apply true
+    alias(libs.plugins.ktlint) apply true
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    autoCorrect = true
+    config.setFrom("$projectDir/config/detekt/detekt.yml")
+    baseline =
+        file("$projectDir/config/detekt/baseline.xml")
+}
+dependencies {
+    detektPlugins(libs.detekt.formatting)
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        xml.required.set(false)
+        txt.required.set(false)
+        sarif.required.set(false)
+        md.required.set(false)
+    }
+}
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    jvmTarget = JavaVersion.VERSION_11.toString()
+}
+tasks.withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
+    jvmTarget = JavaVersion.VERSION_11.toString()
+}
+
+task("staticCheck") {
+    afterEvaluate {
+        val lintDependencies = subprojects.mapNotNull { "${it.name}:lint" }
+        dependsOn(lintDependencies + listOf("ktlintCheck", "detekt"))
+    }
 }
 true // Needed to make the Suppress annotation work for the plugins block
